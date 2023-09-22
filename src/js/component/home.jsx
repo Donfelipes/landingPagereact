21e4b566364690e827ac0b7ccc66f3bbd68ef7a1
Felipe <130509179+Donfelipes@users.noreply.github.com>
2023-09-22 17:51:53 +0000

    End

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,21 +1,24 @@
 import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-import { Navbar } from "./Navbar"
+import { Navbar } from "./Navbar";
 import { Jumbotron } from "./Jumbotron";
 import { Card } from "./Card";
 import { Footer } from "./Footer";
-//create your first component
+
 const Home = () => {
-	return (
-		<>
-		<Navbar/>
-		<Jumbotron/>
-		<Card/>
-		<Footer/>
-		</>
-	);
+  return (
+    <>
+    <Navbar />
+    <Jumbotron />
+	<div className="cards container">
+	<Card/>
+	<Card/>
+	<Card/>
+	<Card/>
+	</div>
+	
+	<Footer />
+    </>
+  );
 };
 
 export default Home;
